c973f076fd2b7d53d8f8b82ab2bb2cd36352dc33
TSAI SHENG-YU <aka4129889@gmail.com>
2023-01-14 20:33:47 +0800

    Initial Commit

---
+++ ChartData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChartData
+//
+//  Created by 蔡勝宇 on 2023/1/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
